28a340314546fe92114451357a9ba29f0dc46e67
iftekher sunny <iftekhersunny@hotmail.com>
2015-08-31 22:39:40 +0600

    Increment version

--- src/Sun/Application.php
+++ src/Sun/Application.php
@@ -11,7 +11,7 @@ class Application extends Container implements ApplicationContract
     /**
      * Planet Framework Version
      */
-    const VERSION = 'v1.0-beta.4';
+    const VERSION = 'v1.0-beta.5';
 
     /**
      * @var \Sun\Routing\Route
