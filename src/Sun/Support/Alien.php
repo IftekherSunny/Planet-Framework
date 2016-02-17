3a9a7eb79e92ca16e23c1641e41d7cdc36f4c09d
iftekher sunny <iftekhersunny@hotmail.com>
2016-02-17 12:17:05 +0600

    Added testing support for the Alien class

--- src/Sun/Support/Alien.php
+++ src/Sun/Support/Alien.php
@@ -2,68 +2,94 @@
 
 namespace Sun\Support;
 
+use Mockery;
 use Exception;
 use ReflectionMethod;
 use ReflectionException;
-use DI\ContainerBuilder;
 use Sun\Support\Exception\BindingException;
-use DI\Definition\Exception\DefinitionException;
-use Sun\Contracts\Support\Alien as AlienContract;
-use Sun\Support\Exception\MethodNotFoundException;
 
-abstract class Alien implements AlienContract
+abstract class Alien
 {
     /**
      * Method name
      *
-     * @var string $method
+     * @var string
      */
     protected static $method;
 
     /**
      * Method arguments
-     * @var array $arguments
+     *
+     * @var array
      */
     protected static $arguments;
 
     /**
-     * To execute method
+     * Application container
      *
-     * @param string $namespace
+     * @var \Sun\Contracts\Container\Container
+     */
+    protected static $container;
+
+    /**
+     * Store mocked class
+     *
+     * @var array
+     */
+    protected static $mocked;
+
+    /**
+     * Execute method of the registered alien
      *
      * @return mixed
      * @throws BindingException
      * @throws MethodNotFoundException
-     * @throws \DI\NotFoundException
      */
-    public static function execute($namespace)
+    protected static function execute()
     {
         try {
-            $instance = app()->make($namespace);
+            $instance = static::getInstance();
 
             $reflectionMethod = new ReflectionMethod($instance, static::$method);
 
             return $reflectionMethod->invokeArgs($instance, static::$arguments);
-        } catch (DefinitionException $e) {
-            throw new BindingException("Binding Error [ ". $e->getMessage() ." ]");
-        } catch (ReflectionException $e) {
-            throw new MethodNotFoundException("Method [ " . static::$method . " ] does not exist.");
+        } catch (Exception $e) {
+            throw new BindingException("Binding Error [ " . $e->getMessage() . " ]");
         }
     }
 
     /**
-     * @param $method
-     * @param $arguments
+     * Set application container
+     */
+    protected static function setContainer()
+    {
+        static::$container = app()->getContainer();
+    }
+
+    /**
+     * Get the class instance of the registered alien
      *
      * @return mixed
      * @throws Exception
      */
-    public static function __callStatic($method, $arguments)
+    protected static function getInstance()
     {
-        static::$method = $method;
-        static::$arguments = $arguments;
+        return isset(static::$mocked[static::registerAlien()])? static::$mocked[static::registerAlien()] : static::$container->make(static::registerAlien());
+    }
 
-        return static::execute(static::registerAlien());
+    /**
+     * Initiate mock expectation for the registered alien
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public static function shouldReceive()
+    {
+        if(!isset(static::$mocked[static::registerAlien()])) {
+            static::$mocked[static::registerAlien()] = Mockery::mock(static::registerAlien());
+        }
+
+        return call_user_func_array([static::$mocked[static::registerAlien()], 'shouldReceive'], func_get_args());
     }
 
     /**
@@ -75,5 +101,26 @@ abstract class Alien implements AlienContract
     protected static function registerAlien()
     {
         throw new Exception('Alien does not implement registerAlien method.');
+    }
+
+    /**
+     * To handle static call dynamically
+     *
+     * @param $method
+     * @param $arguments
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        static::$method = $method;
+        static::$arguments = $arguments;
+
+        if(is_null(static::$container)) {
+            static::setContainer();
+        }
+
+        return static::execute();
     }
 }
